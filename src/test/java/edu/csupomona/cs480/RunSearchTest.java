1ea62029a2d99a905f3cdf6cf109d925f3599e6c
dcoldivar <dcoldivar@cpp.edu>
2016-07-12 11:52:27 -0700

    J Unit Test Class Added
    Checks to see if yelp search results are null

---
+++ src/test/java/edu/csupomona/cs480/RunSearchTest.java
@@ -0,0 +1,28 @@
+package edu.csupomona.cs480;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.csupomona.cs480.yelpV2.RunSearch;
+import edu.csupomona.cs480.yelpV2.YelpSearchResult;
+
+//Dennis Coldivar
+public class RunSearchTest {
+	
+//	public void setup(){
+//		
+//	}
+		
+	//Simple test to make sure Yelp search results does not come back as null
+	@Test
+	public void testRunSearch(){
+		RunSearch rs = new RunSearch();
+		YelpSearchResult results = new YelpSearchResult();
+		results = rs.start();
+		Assert.assertNotNull(results);
+		
+		
+	}
+	
+	
+}
