a60e9808fb23d26c1e70c27bb2237e9b8cdf3b5a
dpemmanuel <David@Davids-MBP.fios-router.home>
2016-07-12 22:50:53 -0700

    Fixed the code

--- src/test/java/edu/csupomona/cs480/GpsTest.java
+++ src/test/java/edu/csupomona/cs480/GpsTest.java
@@ -1,22 +1,24 @@
 package edu.csupomona.cs480;
 import static org.junit.Assert.*;
 import junit.framework.Assert;
-
+ 
 import org.junit.Test;
-
+ 
 import edu.csupomona.cs480.data.GpsProduct;
-// David Emmanuel
-public class GpsTest {
-
-	//@SuppressWarnings("deprecation")
-	//@Test
-	public void testGpsTest() {
-		/*GpsTest gp = new GpsTest();
-		GpsProduct product = new GpsProduct();
-		
-		// Tests to see if it is returning a String;
-		Assert.assertEquals(true, (product.getPrice()).getClass().equals(String.class) ); 
-	}*/
-	int i = 0;
-	}
-}
+ // David Emmanuel
+ public class GpsTest {
+ 
+ 	@Test
+ 	public void testGpsTest() {
+ 		
+ 		GpsProduct product = new GpsProduct();
+ 		// Set up price with initial value
+ 		product.setPrice("5000");
+ 		
+ 		// Tests to see if get price method returns expected string value;
+ 		Assert.assertEquals("5000", product.getPrice());
+ 		
+ 		
+	
+ 	}
+ }
