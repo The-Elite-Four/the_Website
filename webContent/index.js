89a0ec1480bb0d1efbf475c0d3502ea59fe1493d
dpemmanuel <David@b98-aruba1-guest-10-110-5-8.wlan.cpp.edu>
2016-07-15 14:31:15 -0700

    Added to html and revised a name

--- webContent/index.js
+++ webContent/index.js
@@ -3,7 +3,7 @@ function viewResturent()
 {
 	var AsianCheckBox = document.getElementById("Asian").checked;
 	var ArabicCheckBox = document.getElementById("Arabic").checked;
-	var MexicanCheckBox = document.getElementById("Mexican").checked;
+	var HispanicCheckBox = document.getElementById("Hispanic").checked;
 	var FastFoodCheckBox = document.getElementById("Fast Food").checked;
 	var ItalianCheckBox = document.getElementById("Italian").checked;
 	var DessertCheckBox = document.getElementById("Dessert").checked;
@@ -16,9 +16,9 @@ function viewResturent()
 	alert("arabic food");
 	}
 
-	if(MexicanCheckBox==true)
+	if(HispanicCheckBox==true)
 	{
-	alert("Mexican food");
+	alert("Hispanic food");
 	}
 
 	if(FastFoodCheckBox==true)
