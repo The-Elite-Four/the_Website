bf3f6ea3ee34c6be6f282f40e1c331f1ced2b24c
dpemmanuel <David@b98-aruba1-guest-10-110-5-8.wlan.cpp.edu>
2016-07-15 14:16:20 -0700

    Added more options

--- webContent/index.js
+++ webContent/index.js
@@ -4,6 +4,9 @@ function viewResturent()
 	var AsianCheckBox = document.getElementById("Asian").checked;
 	var ArabicCheckBox = document.getElementById("Arabic").checked;
 	var MexicanCheckBox = document.getElementById("Mexican").checked;
+	var FastFoodCheckBox = document.getElementById("Fast Food").checked;
+	var ItalianCheckBox = document.getElementById("Italian").checked;
+	var DessertCheckBox = document.getElementById("Dessert").checked;
 	if(AsianCheckBox==true)
 		{
 		alert("asian food");
@@ -16,6 +19,21 @@ function viewResturent()
 	if(MexicanCheckBox==true)
 	{
 	alert("Mexican food");
+	}
+
+	if(FastFoodCheckBox==true)
+	{
+	alert("Fast Food");
+	}
+
+	if(ItalianCheckBox==true)
+	{
+	alert("Italian");
+	}
+
+	if(DessertCheckBox==true)
+	{
+	alert("Dessert");
 	}
 	
 	//document.getElementbyID("r").innerHTML="view Asian Foods";
