e894fb8f5aa6034e2f41905ce9dd6f1b3582c7be
Nawaf Alsufiani <nawafh1986@hotmail.com>
2016-07-15 15:10:37 -0700

    Junit Test Assignment 6
    JUnit test was added

---
+++ src/test/java/edu/csupomona/cs480/LocationTest.java
@@ -0,0 +1,32 @@
+package edu.csupomona.cs480;
+
+import org.junit.Test;
+
+import edu.csupomona.cs480.data.GpsProduct;
+import edu.csupomona.cs480.yelpV2.Location;
+import junit.framework.Assert;
+
+public class LocationTest 
+{
+
+
+	
+	///////////
+	@Test
+ 	public void testLocationTest() {
+ 		
+ 		Location location = new Location();
+ 		// Set up price with initial value
+ 		location.setCity("irvine");
+ 		
+ 		// Tests to see if get price method returns expected string value;
+ 		Assert.assertEquals("irvine",location.getCity());
+ 		
+ 		
+	
+ 	}
+	
+
+	
+	
+}
