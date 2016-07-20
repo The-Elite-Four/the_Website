4bd7c92b73110e614dbe834f87a929ba0b036066
Jose Gutierrez <triforcegold@hotmail.com>
2016-07-19 21:42:32 -0700

    Testing changes to webserver

--- src/main/java/edu/csupomona/cs480/App.java
+++ src/main/java/edu/csupomona/cs480/App.java
@@ -23,13 +23,17 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan("edu.csupomona.cs480")
 public class App extends WebMvcConfigurerAdapter {
 
+    private static final String[] RESOURCE_LOCATIONS = {"classpath:/static/", "classpath:/webContent/"};
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+       /* registry.addResourceHandler("/static/**").addResourceLocations("/static/");
         registry.addResourceHandler("/css/**").addResourceLocations("/webContent/").setCachePeriod(31556926);
         registry.addResourceHandler("/img/**").addResourceLocations("/webContent/").setCachePeriod(31556926);
-        registry.addResourceHandler("/js/**").addResourceLocations("/webContent/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/webContent/").setCachePeriod(31556926);*/
+        if(!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(RESOURCE_LOCATIONS);
+        }
     }
 
     @Override
