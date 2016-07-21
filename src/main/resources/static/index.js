7f837099a610c8070c591aab785f8366f3843580
Nawaf Alsufiani <nawafh1986@hotmail.com>
2016-07-21 06:34:41 -0700

    serach engine
    I managed to make the search engine read any key to search by it. For
    example search by phone number, type of food.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,48 +1,47 @@
-function search() {
-	var text = document.getElementById('text').value.toUpperCase();
-	var passIn = "/cs480/formattedResults" //This should ideally return a 404 or some error and NOT this.
-	if(text == "ASIAN") { //If text captured from search box is "Asian", execute. Should be a contains "Asian" though
-		//passIn = "/cs480/AsianFood";
-		//LOAD FROM AsianFood.txt and display contents to html,
-		//contnet is sorted as follows:
-		//NAME
-		//Address
-		//phone
-		//SOME ADDRESSES ARE SUPER WORTHLESS
 
-		//ALSO: the if(text == "Asian") line should also have an || text == "Vietnamese"
-		//the point is that it also pulls other asian foods like korean, japanese, sushi, etc.
-	} else if(text == "BREAKFAST") {
-		//passIn = "/cs480/Breakfast";
-		//LOAD FROM Breakfast.txt and display contents to html,
-		//contnet is sorted as follows:
-		//NAME
-		//Address
-		//phone
-		//SOME ADDRESSES ARE SUPER WORTHLESS
-	} else if(text == "MEXICAN") {
-		//passIn = "/cs480/Mexican";
-	}
-	/*var xhttp = new XMLHttpRequest();
-	xhttp.onreadystateChange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-	      document.getElementById(passIn).innerHTML = xhttp.responseText;
-	    }
-	};
-	xhttp.open("GET", passIn, true);
-	xhttp.send();*/
-	alert(passIn); //THIS IS HERE PURELY TO ENSURE MY passIn IS INITIALLIZED PROPERLY, DO NOT LEAVE IN FINAL BUILD
-}
-
-function a()
-{
-	alert("al");
+function search()
+{   var text = document.getElementById("text").value;
+     document.getElementById("demo").innerHTML="";
+	var d = "";
+	var counter =0;
 	  var xhttp = new XMLHttpRequest();
 	  xhttp.onreadystatechange = function() {
 	    if (xhttp.readyState == 4 && xhttp.status == 200) {
-	      document.getElementById("demo").innerHTML = xhttp.responseText;
+	    	d = xhttp.responseText;
+	     
+	     var element = d.split(",");
+	     var t =0;
+	     var para;
+	     var node;
+	     var e;
+	     var para2;
+	     var node2;
+	     var e2;
+	      for(var i=0;i<element.length;i++)
+	    	 {
+	    	     t=element[i].indexOf(text);
+	    	     if(t!=-1)
+	    	    	 {
+                        counter++;
+                       para = document.createElement("p");
+                       node = document.createTextNode(element[i]);
+                        para.appendChild(node);
+                        e = document.getElementById("demo");
+                        e.appendChild(para);
+                      
+                        
+	    	    	 }
+	    	 }
+	      if(counter==0)
+	    	  {
+	    	  alert("not found. Please try again");
+	    	  }
+	       
 	    }
 	  };
-	  xhttp.open("GET", "/cs480/testapi", true);
+	  
+	  xhttp.open("GET", "/cs480/formattedresults", true);
 	  xhttp.send();
+	
+
 }
